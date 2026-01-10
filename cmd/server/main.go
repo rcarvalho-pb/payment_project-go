5975147825c9cf38ea7d017113b733c0b401c912
Ramon Carvalho <rcarvalho.pb@gmail.com>
2026-01-09 23:25:58 -0300

    feat: invoice repository

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -23,19 +23,26 @@ func main() {
 	invoiceRepository := sqlite.NewInvoiceRepository(db)
 	// paymentRepository := sqlite.NewPaymentRepository(db)
 
-	logger.Info("finding invoice: inv-123", nil)
-	inv, err := invoiceRepository.FindByID("inv-456")
+	inv := invoice.NewInvoice("inv-789", 10000)
+	err := invoiceRepository.Save(inv)
+	if err != nil {
+		logger.Error("error saving invoice: "+err.Error(), nil)
+	}
+
+	logger.Info("finding invoice: inv-789", nil)
+	inv, err = invoiceRepository.FindByID("inv-789")
 	if err != nil {
 		logger.Error("error finding by id: "+err.Error(), nil)
 	}
+	log.Printf("%+v\n", inv)
 
 	logger.Info("updating status", nil)
-	err = invoiceRepository.UpdateStatus("inv-456", invoice.StatusPaid)
+	err = invoiceRepository.UpdateStatus("inv-789", invoice.StatusCanceled)
 	if err != nil {
 		logger.Error("error updating status by id: "+err.Error(), nil)
 	}
 
-	inv, err = invoiceRepository.FindByID("inv-456")
+	inv, err = invoiceRepository.FindByID("inv-789")
 	if err != nil {
 		logger.Error("error finding by id: "+err.Error(), nil)
 	}
--- internal/infrastructure/persistence/sqlite/invoice_repository.go
+++ internal/infrastructure/persistence/sqlite/invoice_repository.go
@@ -15,23 +15,15 @@ func NewInvoiceRepository(db *sqlx.DB) *InvoiceRepository {
 	return &InvoiceRepository{db}
 }
 
-// type Invoice struct {
-// 	ID        string    `db:"id"`
-// 	Amount    int64     `db:"amount"`
-// 	Status    Status    `db:"status"`
-// 	CreatedAt time.Time `db:"created_at"`
-// 	DueDate   time.Time `db:"due_date"`
-// }
-
 func (r *InvoiceRepository) Save(inv *invoice.Invoice) error {
-	stmt := `INSERT INTO invoices (id, amount, status, created_at, due_date, updated_at) VALUES ($1, $2, $3, $4, $5, $6)`
+	stmt := `INSERT INTO invoices (id, amount, status, created_at, due_date, updated_at) VALUES (?, ?, ?, ?, ?, ?)`
 	_, err := r.db.Exec(stmt, inv.ID, inv.Amount, inv.Status, inv.CreatedAt, inv.DueDate, time.Now())
 	return err
 }
 
 func (r *InvoiceRepository) FindByID(id string) (*invoice.Invoice, error) {
 	var inv invoice.Invoice
-	query := `SELECT * FROM invoices WHERE id = $1`
+	query := `SELECT * FROM invoices WHERE id = ?`
 	err := r.db.Get(&inv, query, id)
 	if err != nil {
 		return nil, err
@@ -50,7 +42,7 @@ func (r *InvoiceRepository) FindAll() ([]*invoice.Invoice, error) {
 }
 
 func (r *InvoiceRepository) UpdateStatus(id string, status invoice.Status) error {
-	stmt := `UPDATE invoices SET status = $1, updated_at = $2 WHERE id = $3`
+	stmt := `UPDATE invoices SET status = ?, updated_at = ? WHERE id = ?`
 	_, err := r.db.Exec(stmt, status, time.Now(), id)
 	return err
 }
